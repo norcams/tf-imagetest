6fccdebbb94f1772bc1dd3841f4f1b1d048ba659
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2018-07-17 12:14:36 +0000

    Added support for outdated images

--- output.tf
+++ output.tf
@@ -3,15 +3,19 @@
 # The replace hack strips this away to we can use access_ip_v6 as an ansible_host
 
 variable "search"  { default = "/\\[(.*)\\]/" }
-variable "replace" { default = "$1" }
+variable "user_search" { default = "/outdated\\ \\((.*)\\)/" }
 
 data "template_file" "ansible_host" {
     template = "$${name} ansible_host=$${ip} ansible_ssh_user=$${user}"
     count = "${length(var.gold_images)*var.count}"
     vars {
-        ip = "${replace(openstack_compute_instance_v2.basic.*.access_ip_v6[count.index], var.search, var.replace)}"
+        ip = "${replace(openstack_compute_instance_v2.basic.*.access_ip_v6[count.index], var.search, "$1")}"
         name = "${openstack_compute_instance_v2.basic.*.name[count.index]}"
-        user = "${var.gold_images[openstack_compute_instance_v2.basic.*.image_name[count.index]]}"
+        user = "${lookup(
+                  var.gold_images,
+                  openstack_compute_instance_v2.basic.*.image_name[count.index],
+                  replace(lower(openstack_compute_instance_v2.basic.*.image_name[count.index]), var.user_search, "$1")
+        )}"
     }
 }
 
